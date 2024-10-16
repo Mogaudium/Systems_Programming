a935c69e9362bdc4fbc3525adc2ad7d94250da5d
Mogaudium <121038646+Mogaudium@users.noreply.github.com>
2024-10-16 16:24:41 +0300

    Add files via upload

---
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Connect = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.Pause = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            //this.trackBar1.Minimum = 0;
+            //this.trackBar1.Maximum = 100;
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Connect.Location = new System.Drawing.Point(62, 149);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(368, 259);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.ItemHeight = 16;
+            this.fileListBox.Location = new System.Drawing.Point(224, 57);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(219, 196);
+            this.fileListBox.TabIndex = 2;
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(224, 259);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(75, 23);
+            this.Pause.TabIndex = 3;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(224, 292);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(219, 56);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.fileListBox);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        // P/Invoke declarations
+        [DllImport("spdll.dll", CharSet = CharSet.Ansi)]
+        private static extern void InitializeClient(IntPtr hwnd);
+
+        [DllImport("spdll.dll", CharSet = CharSet.Ansi)]
+        private static extern void ConnectToServer(string ipAddress, int port);
+
+        [DllImport("spdll.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr GetFileList(out int count);
+
+        [DllImport("spdll.dll", CharSet = CharSet.Unicode)]
+        private static extern void RequestAudioTrack(string trackName);
+
+        [DllImport("spdll.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void PauseStreaming(bool pause);
+
+        [DllImport("spdll.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void SetVolume(float volume);
+
+        [DllImport("spdll.dll")]
+        private static extern void StreamAudio();
+
+        [DllImport("spdll.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void StartStreaming();
+
+        private System.Windows.Forms.Timer volumeUpdateTimer;
+        private float currentVolume = 0.5f;
+        private bool isPaused = false;
+        private bool isPlaying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeClient(this.Handle);
+
+            // Initialize the volume update timer
+            volumeUpdateTimer = new System.Windows.Forms.Timer
+            {
+                Interval = 100 // Update every 100 milliseconds
+            };
+            volumeUpdateTimer.Tick += VolumeUpdateTimer_Tick;
+            volumeUpdateTimer.Start();
+
+            // Initialize volume slider
+            trackBar1.Value = (int)(currentVolume * trackBar1.Maximum);
+            trackBar1.Scroll += volumeSlider_Scroll;
+
+            // Bind Pause button click event
+            Pause.Click += Pause_Click;
+        }
+
+        private void VolumeUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Call SetVolume function from the DLL to update the volume
+            SetVolume(currentVolume);
+        }
+
+        // Add the received available audio files
+        private void PopulateFileList()
+        {
+            fileListBox.Items.Clear();
+            IntPtr fileListPtr = GetFileList(out int count);
+            string fileList = Marshal.PtrToStringUni(fileListPtr);
+            string[] files = fileList.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            fileListBox.Items.AddRange(files);
+        }
+
+        // Connects to the server and populates the list
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            ConnectToServer("127.0.0.1", 54000);
+            PopulateFileList();
+        }
+
+
+        // Plays the selected audio file
+        private void Play_Click(object sender, EventArgs e)
+        {
+            if (fileListBox.SelectedItem != null)
+            {
+                if (!isPlaying)
+                {
+                    // Start new playback
+                    string selectedFile = fileListBox.SelectedItem.ToString();
+                    Task.Run(() => {
+                        RequestAudioTrack(selectedFile);
+                        StartStreaming();
+                    });
+                    isPlaying = true;
+                    isPaused = false;
+                }
+                else if (isPaused)
+                {
+                    // Resume playback
+                    PauseStreaming(false);
+                    isPaused = false;
+                }
+            }
+        }
+
+        // Pause the audio playback
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                    isPaused = !isPaused;
+                PauseStreaming(isPaused);
+            }
+        }
+
+        // Changes the volume of audio playback
+            private void volumeSlider_Scroll(object sender, EventArgs e)
+        {
+            currentVolume = trackBar1.Value / (float)trackBar1.Maximum;
+
+            SetVolume(currentVolume);
+        }
+    }
+}
